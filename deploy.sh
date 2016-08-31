c3d0f88ab24d356d2b2d68f9202add6363a5ac97
Bocinsky <bocinsky@Bocinskys-MacBook-Pro.local>
2016-08-30 18:53:43 -0600

    Added tabbed browsing

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/expect
+
+set user [lindex $argv 0]; # Grab the first command line parameter
+set pass [lindex $argv 1]; # Grab the first command line parameter
+
+spawn rsync -avzr --size-only --delete ./ $user@macaw.crowcanyon.org:/srv/shiny-server/PFP_Shiny/ 
+expect "password:"
+send "$pass\n"
+expect eof
+if [catch wait] {
+    puts "rsync failed"
+    exit 1
+}
+exit 0
