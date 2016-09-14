fddd59e778870ec9a7a6dccc77ba96438652128d
Bocinsky <bocinsky@Bocinskys-MacBook-Pro.local>
2016-09-14 07:44:59 -0600

    Set ranges for years

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set user [lindex $argv 0]; # Grab the first command line parameter
 set pass [lindex $argv 1]; # Grab the first command line parameter
 
-spawn rsync -avzr --size-only --delete ./ $user@macaw.crowcanyon.org:/srv/shiny-server/pfp_shiny/ 
+spawn rsync -avzr --size-only --delete ./ $user@macaw.crowcanyon.org:/srv/shiny-server/pfp/ 
 expect "password:"
 send "$pass\n"
 expect eof
